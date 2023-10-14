3141dcf683e615ff7260f6735e001e17dcd21d3f
Rijo2 <141557328+Rijo2@users.noreply.github.com>
2023-10-14 10:45:43 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,14 @@
+-- SELECT statement
 
+-- select all (*) columns from table
+SELECT * FROM employees;
+SELECT * FROM food_store;
+SELECT * FROM locations;
+SELECT * FROM suppliers;
+
+-- select some (3) columns of table
+SELECT
+	employee_id,
+	first_name,
+	second_name
+FROM employees;
