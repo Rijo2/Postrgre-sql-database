b2679eed3ee1152a44f19c01a8312455c9aecfe6
Rijo2 <141557328+Rijo2@users.noreply.github.com>
2023-10-15 10:16:28 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -120,3 +120,483 @@ SELECT
 	EXTRACT(MONTH FROM hire_date) AS month,
 	EXTRACT(DAY FROM hire_date) AS day
 FROM employees;
+
+--=========================================================
+
+-- UPPER, LOWER, LENGTH, TRIM
+
+-- Uppercase first and second names
+
+SELECT
+	first_name,
+	UPPER(first_name) AS first_name_upper,
+	second_name,
+	UPPER(second_name) AS second_name_upper
+FROM employees;
+
+-- Lowercase first and last names
+
+SELECT
+	first_name,
+	LOWER(first_name) AS first_name_upper,
+	second_name,
+	LOWER(second_name) AS second_name_upper
+FROM employees;
+
+-- Return the email and the length of emails
+SELECT
+	email,
+	LENGTH(email) AS email_length
+FROM employees;
+
+-- TRIM
+SELECT
+    LENGTH('     HELLO     ') AS hello_with_spaces,
+LENGTH('HELLO') AS hello_no_spaces,
+    LENGTH(TRIM('     HELLO     ')) AS hello_trimmed;
+
+-- Concatenation, Boolean expressions, wildcards
+
+-- Concatenate first and last names to create full names
+SELECT
+	first_name,
+	second_name,
+	first_name || ' ' || second_name AS full_name
+FROM employees;
+
+-- Concatenate columns to create a sentence
+SELECT 
+	first_name || ' ' || second_name || ' makes ' || salary
+FROM employees;
+
+-- Boolean expressions
+-- if the person makes less than 50k, then true, otherwise false
+SELECT
+	first_name || ' ' || second_name AS full_name,
+	(salary < 50000) AS less_than_50k
+FROM employees;
+
+-- if the person is a female and makes less than 50k, then true, otherwise false
+SELECT
+	first_name || ' ' || second_name AS full_name,
+	(salary < 50000 AND gender = 'F') AS less_than_50k_female
+FROM employees;
+
+-- Boolean expressions with wildcards (% subString)
+-- if email has '.com', return true, otherwise false
+
+SELECT
+	email,
+	(email like '%.com%') AS dotcom_flag
+FROM employees;
+
+SELECT
+	email,
+	(email like '%.gov%') AS dotgov_flag
+FROM employees;
+
+-- return only government employees
+select
+  first_name || ' ' || second_name as full_name,
+  email as gov_email
+from employees
+where email like '%.gov%';
+
+--==========================================================
+
+-- SUBSTRING, POSITION, COALESCE
+
+-- SUBSTRING
+-- Get the email from the 5th character
+SELECT 
+	email,
+	SUBSTRING(email FROM 5)
+FROM employees;
+
+-- POSITION
+-- Find the position of '@' in the email
+SELECT 
+	email,
+	POSITION('@' IN email)
+FROM employees;
+
+-- SUBSTRING & POSITION to find the email client
+SELECT 
+	email,
+	SUBSTRING(email FROM POSITION('@' IN email))
+FROM employees;
+
+SELECT 
+	email,
+	SUBSTRING(email FROM POSITION('@' IN email) + 1)
+FROM employees;
+
+-- COALESCE to fill missing emails with custom value
+
+SELECT 
+	email,
+	COALESCE(email, 'NO EMAIL PROVIDED')
+FROM employees;
+
+--===================================================
+
+-- MIN, MAX, AVG, SUM, COUNT
+
+-- Select the minimum salary
+SELECT MIN(salary) as min_sal
+FROM employees;
+
+-- Select the maximum salary
+SELECT MAX(salary) as max_sal
+FROM employees;
+
+-- Select difference between maximum and minimum salary
+SELECT MAX(salary) - MIN(salary)
+FROM employees;
+
+-- Select the average salary
+SELECT AVG(salary)
+FROM employees;
+
+-- Round average salary to nearest integer
+SELECT ROUND(AVG(salary),0)
+FROM employees;
+
+-- Sum up the salaries
+SELECT SUM(salary)
+FROM employees;
+
+-- Count the number of entries
+SELECT COUNT(*)
+FROM employees;
+
+SELECT COUNT(salary)
+FROM employees;
+
+SELECT COUNT(email)
+FROM employees;
+
+-- summary
+
+SELECT
+	MIN(salary) as lowest_salary,
+	MAX(salary) as highest_salary,
+	MAX(salary) - MIN(salary) as salary_difference,
+	round(avg(salary), 0) as average_salary,
+	sum(salary) as total_salary,
+	count(*) as number_of_employees
+FROM employees;
+
+--=========================================================
+
+-- GROUP BY & HAVING
+
+-- Return the number of employees for each coffeeshop
+SELECT
+  shop_id,
+	COUNT(employee_id)
+FROM employees
+GROUP BY shop_id;
+
+-- Return the total salaries for each coffeeshop
+SELECT
+  shop_id,
+	SUM(salary)
+FROM employees
+GROUP BY shop_id;
+
+-- Return the number of employees, the avg & min & max & total salaries for each coffeeshop
+SELECT
+	shop_id, 
+	COUNT(*) AS num_of_emp,
+	ROUND(AVG(salary), 0) AS avg_sal,
+	MIN(salary) AS min_sal,
+    MAX(salary) AS max_sal,
+	SUM(salary) AS total_sal
+FROM employees
+GROUP BY shop_id
+ORDER BY num_of_emp DESC;
+
+-- HAVING
+-- After GROUP BY, return only the coffeeshops with more than 2 employees
+SELECT
+	shop_id, 
+	COUNT(*) AS num_of_emp,
+	ROUND(AVG(salary), 0) AS avg_sal,
+	MIN(salary) AS min_sal,
+    MAX(salary) AS max_sal,
+	SUM(salary) AS total_sal
+FROM employees
+GROUP BY shop_id
+HAVING COUNT(*) > 2  -- filter, alter "where" after "group by"
+ORDER BY num_of_emp DESC;
+
+-- After GROUP BY, return only the coffeeshops with a minimum salary of less than 100000
+SELECT
+	shop_id, 
+	COUNT(*) AS num_of_emp,
+	ROUND(AVG(salary), 0) AS avg_sal,
+	MIN(salary) AS min_sal,
+    MAX(salary) AS max_sal,
+	SUM(salary) AS total_sal
+FROM employees
+GROUP BY shop_id
+HAVING MIN(salary) < 100000
+ORDER BY num_of_emp DESC;
+
+--===========================================================
+
+-- CASE, CASE with GROUP BY, and CASE for transposing data
+
+-- CASE
+-- If pay is less than 50k, then low pay, otherwise high pay
+SELECT
+	employee_id,
+	first_name || ' ' || second_name as full_name,
+	salary,
+	CASE
+		WHEN salary < 50000 THEN 'low pay'
+		WHEN salary >= 50000 THEN 'high pay'
+		ELSE 'no pay'
+	END as pay_category
+FROM employees
+ORDER BY salary DESC;
+
+-- If pay is less than 20k, then low pay
+-- if between 20k-50k inclusive, then medium pay
+-- if over 50k, then high pay
+SELECT
+	employee_id,
+	first_name || ' ' || second_name as full_name,
+	salary,
+	CASE
+		WHEN salary < 20000 THEN 'low pay'
+		WHEN salary BETWEEN 20000 and 50000 THEN 'medium pay'
+		WHEN salary > 50000 THEN 'high pay'
+		ELSE 'no pay'
+	END as pay_category
+FROM employees
+ORDER BY salary DESC;
+
+-- CASE & GROUP BY 
+-- Return the count of employees in each pay category
+SELECT a.pay_category, COUNT(*)
+FROM(
+	SELECT
+		employee_id,
+	    first_name || ' ' || second_name as full_name,
+		salary,
+    CASE
+			WHEN salary < 20000 THEN 'low pay'
+			WHEN salary BETWEEN 20000 and 50000 THEN 'medium pay'
+			WHEN salary > 50000 THEN 'high pay'
+			ELSE 'no pay'
+		END as pay_category
+	FROM employees
+	ORDER BY salary DESC
+) a
+GROUP BY a.pay_category;
+
+-- Transpose above
+SELECT
+	SUM(CASE WHEN salary < 20000 THEN 1 ELSE 0 END) AS low_pay,
+	SUM(CASE WHEN salary BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END) AS medium_pay,
+	SUM(CASE WHEN salary > 50000 THEN 1 ELSE 0 END) AS high_pay
+FROM employees;
+
+--================================================
+
+-- JOIN
+
+-- Inserting values just for JOIN exercises
+INSERT INTO locations VALUES (4, 'Paris', 'France');
+INSERT INTO food_store VALUES (6, 'Happy Brew', NULL);
+
+-- Checking the values we inserted
+SELECT * FROM food_store;
+SELECT * FROM locations;
+
+-- "INNER JOIN" same as just "J0iN"
+SELECT 
+	s.shop_name,
+	l.city,
+	l.country
+FROM (
+	food_store s
+	inner JOIN locations as l
+	ON s.city_id = l.city_id
+);
+
+SELECT
+  s.shop_name,
+  l.city,
+  l.country
+FROM
+  food_store s
+  JOIN locations l
+  ON s.city_id = l.city_id;
+
+-- LEFT JOIN
+SELECT
+  s.shop_name,
+  l.city,
+  l.country
+FROM
+	food_store s
+	LEFT JOIN locations l
+	ON s.city_id = l.city_id;
+
+-- RIGHT JOIN
+SELECT
+  s.shop_name,
+  l.city,
+  l.country
+FROM
+	food_store s
+	RIGHT JOIN locations l
+	ON s.city_id = l.city_id;
+
+-- FULL OUTER JOIN
+SELECT
+  s.shop_name,
+  l.city,
+  l.country
+FROM
+	food_store s
+	FULL OUTER JOIN locations l
+	ON s.city_id = l.city_id;
+
+-- Delete the values we created just for the JOIN exercises
+DELETE FROM locations WHERE city_id = 4;
+DELETE FROM food_store WHERE shop_id = 6;
+
+--========================================================
+
+-- UNION (to stack data on top each other)
+
+-- Return all cities and countries
+SELECT city FROM locations
+UNION
+SELECT country FROM locations;
+
+-- UNION removes duplicates
+SELECT country FROM locations
+UNION
+SELECT country FROM locations;
+
+-- UNION ALL keeps duplicates
+SELECT country FROM locations
+UNION ALL
+SELECT country FROM locations;
+
+-- Return all food store names, cities and countries
+SELECT shop_name FROM food_store
+UNION
+SELECT city FROM locations
+UNION
+SELECT country FROM locations;
+
+--=================================================
+
+-- Subqueries
+
+-- Basic subqueries with subqueries in the FROM clause
+SELECT *
+FROM (
+	SELECT *
+	FROM employees
+	where shop_id IN (3,4)
+) as a;
+
+SELECT
+  a.employee_id,
+	a.first_name,
+	a.second_name
+FROM (
+	SELECT *
+	FROM employees
+	where shop_id IN (3,4)
+) a;
+
+-- Basic subqueries with subqueries in the SELECT clause
+SELECT
+	first_name, 
+	second_name, 
+	salary,
+	(
+		SELECT MAX(salary)
+		FROM employees
+		LIMIT 1
+	) max_sal
+FROM employees;
+
+SELECT
+	first_name, 
+	second_name, 
+	salary,
+	(
+		SELECT ROUND(AVG(salary), 0)
+		FROM employees
+		LIMIT 1
+	) avg_sal
+FROM employees;
+
+SELECT
+	first_name, 
+	second_name, 
+	salary, 
+	salary - ( -- avg_sal
+		SELECT ROUND(AVG(salary), 0)
+		FROM employees
+		LIMIT 1
+	) avg_sal_diff
+FROM employees;
+
+-- Subqueries in the WHERE clause
+-- Return all US coffee shops
+SELECT * 
+FROM food_store
+WHERE city_id IN ( -- US city_id's
+	SELECT city_id
+	FROM locations
+	WHERE country = 'United States'
+);
+
+-- Return all employees who work in US coffee shops
+SELECT *
+FROM employees
+WHERE shop_id IN ( -- US coffeeshop_id's
+	SELECT shop_id 
+	FROM food_store
+	WHERE city_id IN ( -- US city-id's
+		SELECT city_id
+		FROM locations
+		WHERE country = 'United States'
+	)
+);
+
+-- Return all employees who make over 35k and work in US food stores 
+SELECT *
+FROM employees
+WHERE salary > 35000 AND shop_id IN ( -- US coffeeshop_id's
+	SELECT shop_id
+	FROM food_store
+	WHERE city_id IN ( -- US city_id's
+		SELECT city_id
+		FROM locations
+		WHERE country = 'United States'
+	)
+);
+
+-- 30 day moving total pay
+-- The inner query calculates the total_salary of employees who were hired "within" the 30-day period before the hire_date of the current employee
+SELECT
+	hire_date,
+	salary,
+	(
+		SELECT SUM(salary)
+		FROM employees e2
+		WHERE e2.hire_date BETWEEN e1.hire_date - 30 AND e1.hire_date
+	) AS pay_pattern
+FROM employees e1
+ORDER BY hire_date;
